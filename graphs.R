a898070b873c7cb3b78db2768ba2eb9bb3bd29f0
Doris <117998658+dorischen0325@users.noreply.github.com>
2025-04-16 08:53:51 +0800

    Update graphs.R

--- graphs.R
+++ graphs.R
@@ -1,4 +1,4 @@
-# Load necessary library
+#Load libraries
 library(ggplot2)
 library(tidyr)
 library(reshape2)
@@ -10,21 +10,21 @@ library(patchwork)
 library(stringr)
 library(cowplot)
 
-# Read data - leading cause of death
+#Read data - leading cause of death
 data_M <- read.csv("Male Leading Cause of Death.csv", check.names = FALSE)
 data_F <- read.csv("Female Leading Cause of Death.csv", check.names = FALSE)
 
-# Convert data from wide to long format
+#Convert data from wide to long format
 data_long_M <- gather(data_M, Year, Deaths, -Cause)
 data_long_F <- gather(data_F, Year, Deaths, -Cause)
 
-# Convert Year to numeric
+#Convert some year and deaths to numeric
 data_long_M$Year <- as.numeric(data_long_M$Year)
 data_long_M$Deaths <- as.numeric(gsub(",", "", data_long_M$Deaths))
 data_long_F$Year <- as.numeric(data_long_F$Year)
 data_long_F$Deaths <- as.numeric(gsub(",", "", data_long_F$Deaths))
 
-# Plot with formatted y-axis, better x-axis, and legend below
+#Plot with formatted y-axis, x-axis, and legend below
 Plot_M <- ggplot(data_long_M, aes(x = Year, y = Deaths, color = Cause, group = Cause)) +
   geom_line(size = 1) +
   scale_y_continuous(labels = scales::comma_format(scale = 1e-3, suffix = "K")) +  # Show y-axis in 1000s
@@ -55,25 +55,24 @@ Plot_F <- ggplot(data_long_F, aes(x = Year, y = Deaths, color = Cause, group = C
         legend.key.size = unit(0.5, "lines"))
 
 combined <- Plot_F + Plot_M
-
-# Save a ggplot with specific dimensions
+#Save a ggplot
 ggsave("Death_Trends.png",combined, width = 8, height = 3.5, dpi = 300)
 
-# Read the CSV file - Lee-Carter mx
+#Read the CSV file - Lee-Carter mx for both genders
 dataF <- read.csv("LCmxF.csv", check.names = FALSE)
 dataM <- read.csv("LCmxM.csv", check.names = FALSE)
 
-# Convert data to long format
+#Convert data to long format
 data_longF <- gather(dataF, Year, Value, -Age)
 data_longM <- gather(dataM, Year, Value, -Age)
 
-# Convert columns to appropriate data types
+#Convert columns to numeric
 data_longF$Year <- as.numeric(data_longF$Year)
 data_longF$Value <- as.numeric(data_longF$Value)
 data_longM$Year <- as.numeric(data_longM$Year)
 data_longM$Value <- as.numeric(data_longM$Value)
 
-# Identify start and end points for labeling
+#Identify start and end points for labeling
 start_pointsF <- data_longF %>% group_by(Age) %>% filter(Year == min(Year))
 end_pointsF <- data_longF %>% group_by(Age) %>% filter(Year == max(Year))
 label_pointsF <- bind_rows(start_pointsF, end_pointsF)
@@ -81,7 +80,7 @@ start_pointsM <- data_longM %>% group_by(Age) %>% filter(Year == min(Year))
 end_pointsM <- data_longM %>% group_by(Age) %>% filter(Year == max(Year))
 label_pointsM <- bind_rows(start_pointsM, end_pointsM)
 
-# Create the plot
+#Create the plots
 Plot_F <- ggplot(data_longF, aes(x = Year, y = Value, color = factor(Age), group = Age)) +
   geom_line(size = 1) +
   scale_y_continuous(labels = function(x) {
@@ -115,8 +114,7 @@ Plot_F <- ggplot(data_longF, aes(x = Year, y = Value, color = factor(Age), group
     legend.title = element_text(face = "bold", size = 10),
     legend.text = element_text(face = "bold", size = 10),
     legend.key.size = unit(0.5, "lines"))
-
-ggsave("mxFemale.png", Plot_F, width = 6, height = 4, dpi = 300)
+#ggsave("mxFemale.png", Plot_F, width = 6, height = 4, dpi = 300)
 
 Plot_M <- ggplot(data_longM, aes(x = Year, y = Value, color = factor(Age), group = Age)) +
   geom_line(size = 1) +
@@ -151,38 +149,36 @@ Plot_M <- ggplot(data_longM, aes(x = Year, y = Value, color = factor(Age), group
     legend.title = element_text(face = "bold", size = 10),
     legend.text = element_text(face = "bold", size = 10),
     legend.key.size = unit(0.5, "lines"))
-
 #ggsave("mxMale.png", Plot_M, width = 6, height = 4, dpi = 300)
-# Save a ggplot with specific dimensions
+# Save a ggplot
 combined <- Plot_F + Plot_M
 ggsave("LCmx.png",combined, width = 8, height = 4, dpi = 300)
 
-# Read the CSV file - Lee-Carter ex
+#Read the CSV file - Lee-Carter ex for both genders
 dataF <- read.csv("LCexF.csv", check.names = FALSE)
 dataM <- read.csv("LCexM.csv", check.names = FALSE)
 
+#Convert data to long format
 data_longF <- dataF %>%
   pivot_longer(cols = -Year, names_to = "Age", values_to = "Value")
-
 data_longM <- dataM %>%
   pivot_longer(cols = -Year, names_to = "Age", values_to = "Value")
 
-# Convert to numeric
+#Convert to numeric
 data_longF$Year <- as.numeric(data_longF$Year)
 data_longF$Value <- as.numeric(data_longF$Value)
 data_longM$Year <- as.numeric(data_longM$Year)
 data_longM$Value <- as.numeric(data_longM$Value)
 
-# Identify start and end points
+#Identify start and end points
 start_pointsF <- data_longF %>% group_by(Age) %>% filter(Year == min(Year))
 end_pointsF   <- data_longF %>% group_by(Age) %>% filter(Year == max(Year))
 label_pointsF <- bind_rows(start_pointsF, end_pointsF)
-
 start_pointsM <- data_longM %>% group_by(Age) %>% filter(Year == min(Year))
 end_pointsM   <- data_longM %>% group_by(Age) %>% filter(Year == max(Year))
 label_pointsM <- bind_rows(start_pointsM, end_pointsM)
 
-# Create the plot
+#Create the plots
 Plot_F <- ggplot(data_longF, aes(x = Year, y = Value, color = factor(Age), group = Age)) +
   geom_line(size = 1) +
   scale_y_continuous(labels = function(x) {
@@ -216,7 +212,6 @@ Plot_F <- ggplot(data_longF, aes(x = Year, y = Value, color = factor(Age), group
     legend.title = element_text(face = "bold", size = 10),
     legend.text = element_text(face = "bold", size = 10),
     legend.key.size = unit(0.5, "lines"))
-
 #ggsave("exFemale.png", Plot_F, width = 6, height = 4, dpi = 300)
 
 Plot_M <- ggplot(data_longM, aes(x = Year, y = Value, color = factor(Age), group = Age)) +
@@ -252,28 +247,28 @@ Plot_M <- ggplot(data_longM, aes(x = Year, y = Value, color = factor(Age), group
     legend.title = element_text(face = "bold", size = 10),
     legend.text = element_text(face = "bold", size = 10),
     legend.key.size = unit(0.5, "lines"))
-
 #ggsave("exMale.png", Plot_M, width = 6, height = 4, dpi = 300)
-# Save a ggplot with specific dimensions
+
+#Save a ggplot
 combined <- Plot_F + Plot_M
 ggsave("LCex.png",combined, width = 8, height = 4, dpi = 300)
 
-# Read the CSV file - DGM ex
+#Read the CSV file - DGM ex for both gender
 data35 <- read.csv("DGM35.csv", check.names = FALSE)
 data55 <- read.csv("DGM55.csv", check.names = FALSE)
 data75 <- read.csv("DGM75.csv", check.names = FALSE)
 
-# Convert data to long format for ggplot
+#Convert data to long format
 data_long35 <- gather(data35, Category, Value, -Year)
 data_long55 <- gather(data55, Category, Value, -Year)
 data_long75 <- gather(data75, Category, Value, -Year)
 
-# Ensure Year is numeric
+#Convert year to numeric
 data_long35$Year <- as.numeric(data_long35$Year)
 data_long55$Year <- as.numeric(data_long55$Year)
 data_long75$Year <- as.numeric(data_long75$Year)
 
-# Split datasets for separate plots
+#Split datasets for separate plots
 life_expectancy_data35 <- data_long35 %>%
   filter(Category %in% c("Female Life Expectancy", "Male Life Expectancy", "Best Practice Life Expectancy"))
 gap_data35 <- data_long35 %>%
@@ -287,7 +282,7 @@ life_expectancy_data75 <- data_long75 %>%
 gap_data75 <- data_long75 %>%
   filter(Category %in% c("Sex Gap", "Best Practice Gap"))
 
-# Identify start and end points for bold labels
+#Identify start and end points for bold labels
 life_expectancy_labels35 <- life_expectancy_data35 %>%
   group_by(Category) %>%
   filter(Year == min(Year) | Year == max(Year))
@@ -307,7 +302,7 @@ gap_labels75 <- gap_data75 %>%
   group_by(Category) %>%
   filter(Year == min(Year) | Year == max(Year))
 
-# Identify last data point for each category to label the category name below the line
+#Identify last data point for each category to label the category name below the line
 life_expectancy_end_labels35 <- life_expectancy_data35 %>%
   group_by(Category) %>%
   filter(Year == max(Year))
@@ -327,7 +322,7 @@ gap_end_labels75 <- gap_data75 %>%
   group_by(Category) %>%
   filter(Year == max(Year))
 
-# Define custom colors
+#Define colors
 life_expectancy_colors <- c("Female Life Expectancy" = "pink", 
                             "Male Life Expectancy" = "blue", 
                             "Best Practice Life Expectancy" = "green")
@@ -335,7 +330,7 @@ life_expectancy_colors <- c("Female Life Expectancy" = "pink",
 gap_colors <- c("Sex Gap" = "purple", "Best Practice Gap" = "orange")
 
 
-# Plot: Life Expectancy
+#Plot life expectancy
 Plot_ex35 <- ggplot(life_expectancy_data35, aes(x = Year, y = Value, color = Category, group = Category)) +
   geom_line(size = 1) +
   scale_color_manual(values = life_expectancy_colors,
@@ -452,7 +447,7 @@ Plot_ex75 <- ggplot(life_expectancy_data75, aes(x = Year, y = Value, color = Cat
     legend.text = element_text(face = "bold", size = 10),
     legend.key.size = unit(0.5, "lines"))
 
-# Plot 2: Gaps with Labels Below Lines
+#Plot gender and best practice gaps
 Plot_gap35 <- ggplot(gap_data35, aes(x = Year, y = Value, color = Category, group = Category)) +
   geom_line(size = 1) +
   scale_color_manual(
@@ -567,6 +562,7 @@ Plot_gap75 <- ggplot(gap_data75, aes(x = Year, y = Value, color = Category, grou
     legend.text = element_text(face = "bold", size = 10),
     legend.key.size = unit(0.5, "lines"))
 
+#save ggplots
 combined35 <- Plot_ex35 + Plot_gap35
 combined55 <- Plot_ex55 + Plot_gap55
 combined75 <- Plot_ex75 + Plot_gap75
